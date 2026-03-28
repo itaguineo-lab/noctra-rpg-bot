6c47965b6e9f5dec7476eb2b7f80021ce2076b14
itaguineo-lab <itaguineo@gmail.com>
2026-03-28 13:22:26 -0300

    Criar utils.js

---
+++ utils.js
@@ -0,0 +1,9 @@
+function progressBar(current, max, size = 10) {
+  const percent = current / max;
+  const filled = Math.round(size * percent);
+  const empty = size - filled;
+
+  return '🟥'.repeat(filled) + '⬜'.repeat(empty);
+}
+
+module.exports = { progressBar };
