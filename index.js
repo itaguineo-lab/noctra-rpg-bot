5e0df58442ae44186db8626102eb9587d6a60661
itaguineo-lab <itaguineo@gmail.com>
2026-03-28 13:32:28 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-
-const { checkLevelUp, xpToNext } = require('./level');
-const { progressBar } = require('./utils');
-const { getPlayer } = require('./players');
-const { updateEnergy, useEnergy } = require('./energy');
-const { fight } = require('./combat');
-
 const { Telegraf, Markup } = require('telegraf');
 const express = require('express');
 
+const { getPlayer } = require('./players');
+const { updateEnergy, useEnergy } = require('./energy');
+const { fight } = require('./combat');
+const { checkLevelUp, xpToNext } = require('./level');
+const { progressBar } = require('./utils');
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// servidor (Render precisa disso)
 const app = express();
 
 app.get('/', (req, res) => {
@@ -25,13 +25,12 @@ bot.start((ctx) => {
     '🌑 NOCTRA RPG\n\nEscolha sua ação:',
     Markup.inlineKeyboard([
       [Markup.button.callback('⚔️ Caçar', 'hunt')],
-      [Markup.button.callback('🧭 Explorar', 'explore')],
       [Markup.button.callback('👤 Status', 'status')],
     ])
   );
 });
 
-// AÇÕES
+// CAÇAR (COMBATE COMPLETO)
 
 bot.action('hunt', async (ctx) => {
   await ctx.answerCbQuery();
@@ -46,7 +45,6 @@ bot.action('hunt', async (ctx) => {
   const result = fight(player);
 
   const leveledUp = checkLevelUp(player);
-
   const xpMax = xpToNext(player.level);
 
   let msg = `⚔️ COMBATE\n\n`;
@@ -72,10 +70,7 @@ bot.action('hunt', async (ctx) => {
   ctx.reply(msg);
 });
 
-bot.action('explore', async (ctx) => {
-  await ctx.answerCbQuery();
-  ctx.reply('🧭 Explorando...');
-});
+// STATUS
 
 bot.action('status', async (ctx) => {
   await ctx.answerCbQuery();
