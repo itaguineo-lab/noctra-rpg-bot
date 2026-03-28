579bc1cabee97760f22dcf853c287503f41230fb
itaguineo-lab <itaguineo@gmail.com>
2026-03-28 15:34:35 -0300

    Atualizar o players.js

--- players.js
+++ players.js
@@ -1,23 +1,57 @@
 const players = {};
 
-function getPlayer(id) {
+// Atributos iniciais por classe (para futura implementação)
+const baseStats = {
+    warrior: { hp: 120, atk: 18, def: 20, crit: 2, agi: 10 },
+    archer: { hp: 100, atk: 22, def: 15, crit: 7, agi: 15 },
+    mage:   { hp: 90,  atk: 20, def: 12, crit: 5, agi: 12 }
+};
+
+function getPlayer(id, className = 'archer') {
     if (!players[id]) {
+        const stats = baseStats[className];
         players[id] = {
-            hp: 100,
-            maxHp: 100,
-            atk: 10,
-            def: 5,
-            gold: 0,
-            xp: 0,
+            // Dados básicos
+            name: null,          // será definido depois
+            class: className,
             level: 1,
+            xp: 0,
+            gold: 0,
             energy: 20,
+            maxEnergy: 20,
             lastEnergy: Date.now(),
-            inventory: [],
+            vip: false,          // será ativado depois
+
+            // Atributos de combate (base)
+            hp: stats.hp,
+            maxHp: stats.hp,
+            atk: stats.atk,
+            def: stats.def,
+            crit: stats.crit,
+            agi: stats.agi,
+
+            // Equipamentos (slots)
             weapon: null,
-            maxInventory: 10
+            armor: null,
+            helmet: null,
+            boots: null,
+            ring: null,
+            necklace: null,
+            bag: null,
+
+            // Almas (2 slots)
+            souls: [null, null],
+
+            // Inventário
+            inventory: [],
+            maxInventory: 20
         };
     }
     return players[id];
 }
 
-module.exports = { getPlayer };
+function savePlayer(id, data) {
+    players[id] = data;
+}
+
+module.exports = { getPlayer, savePlayer };
