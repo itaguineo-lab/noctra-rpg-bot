5ef429a1e0cd750128f7cc06706416daad307b8c
itaguineo-lab <itaguineo@gmail.com>
2026-03-28 12:38:02 -0300

    Criar index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const { Telegraf, Markup } = require('telegraf');
+const express = require('express');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('NOCTRA RPG ONLINE');
+});
+
+app.listen(process.env.PORT || 3000);
+
+// MENU
+
+bot.start((ctx) => {
+  ctx.reply(
+    '🌑 NOCTRA RPG\n\nEscolha sua ação:',
+    Markup.inlineKeyboard([
+      [Markup.button.callback('⚔️ Caçar', 'hunt')],
+      [Markup.button.callback('🧭 Explorar', 'explore')],
+      [Markup.button.callback('👤 Status', 'status')],
+    ])
+  );
+});
+
+// AÇÕES
+
+bot.action('hunt', async (ctx) => {
+  await ctx.answerCbQuery();
+  ctx.reply('⚔️ Você encontrou um inimigo!');
+});
+
+bot.action('explore', async (ctx) => {
+  await ctx.answerCbQuery();
+  ctx.reply('🧭 Explorando...');
+});
+
+bot.action('status', async (ctx) => {
+  await ctx.answerCbQuery();
+  ctx.reply('👤 HP: 100 | ATK: 10 | DEF: 5');
+});
+
+bot.launch();
