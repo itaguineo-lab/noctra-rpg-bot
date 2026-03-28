874817b4397c0598b718bfec94167f55811d5fb7
itaguineo-lab <itaguineo@gmail.com>
2026-03-28 17:21:34 -0300

    Atualizar o level.js

--- level.js
+++ level.js
@@ -1,5 +1,5 @@
 function xpToNext(level) {
-    // Fórmula mais realista: level * 100 + (level-1)^2 * 20
+    // Fórmula: level * 100 + (level-1)^2 * 20
     return Math.floor(level * 100 + Math.pow(level - 1, 2) * 20);
 }
 
@@ -8,14 +8,14 @@ function checkLevelUp(player) {
     while (player.xp >= xpToNext(player.level)) {
         player.xp -= xpToNext(player.level);
         player.level++;
-        // Aumenta atributos
+        // Aumenta atributos (a recalculateStats fará o resto)
         player.maxHp += 8;
         player.hp += 8;
         player.atk += 2;
         player.def += 1;
         player.crit += 0.5;
         player.agi += 0.5;
-        // Arredondar para evitar decimais feios
+        // Arredondar
         player.crit = Math.round(player.crit * 10) / 10;
         player.agi = Math.round(player.agi * 10) / 10;
         up = true;
