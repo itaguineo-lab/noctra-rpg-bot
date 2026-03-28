22855ea3d3cc40097ed744b57159d8353ea052ed
itaguineo-lab <itaguineo@gmail.com>
2026-03-28 15:29:07 -0300

    Atualizar o level.js

--- level.js
+++ level.js
@@ -1,22 +1,19 @@
 function xpToNext(level) {
-  return level * 50;
+    return level * 50; // temporário, depois balancear
 }
 
 function checkLevelUp(player) {
-  let up = false;
-
-  while (player.xp >= xpToNext(player.level)) {
-    player.xp -= xpToNext(player.level);
-    player.level += 1;
-
-    player.maxHp += 10;
-    player.atk += 2;
-    player.def += 1;
-
-    up = true;
-  }
-
-  return up;
+    let up = false;
+    while (player.xp >= xpToNext(player.level)) {
+        player.xp -= xpToNext(player.level);
+        player.level++;
+        player.maxHp += 10;
+        player.hp += 10;      // também recupera HP
+        player.atk += 2;
+        player.def += 1;
+        up = true;
+    }
+    return up;
 }
 
 module.exports = { checkLevelUp, xpToNext };
