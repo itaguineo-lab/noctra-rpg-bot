a6731d0c366c01328943be659a89389bcbc33bf6
itaguineo-lab <itaguineo@gmail.com>
2026-03-28 15:28:21 -0300

    Atualizar o players.js

--- players.js
+++ players.js
@@ -1,24 +1,23 @@
 const players = {};
 
 function getPlayer(id) {
-  if (!players[id]) {
-    players[id] = {
-      hp: 100,
-      maxHp: 100,
-      atk: 10,
-      def: 5,
-      gold: 0,
-      xp: 0,
-      level: 1,
-      energy: 20,
-      lastEnergy: Date.now(),
-
-      inventory: [],
-      weapon: null,
-      maxInventory: 10
-    };
-  }
-  return players[id];
+    if (!players[id]) {
+        players[id] = {
+            hp: 100,
+            maxHp: 100,
+            atk: 10,
+            def: 5,
+            gold: 0,
+            xp: 0,
+            level: 1,
+            energy: 20,
+            lastEnergy: Date.now(),
+            inventory: [],
+            weapon: null,
+            maxInventory: 10
+        };
+    }
+    return players[id];
 }
 
 module.exports = { getPlayer };
