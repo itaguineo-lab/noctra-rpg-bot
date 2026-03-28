0b0d90df90a2ff48624df58910a66002d9eefa0b
itaguineo-lab <itaguineo@gmail.com>
2026-03-28 15:31:08 -0300

    Atualizar o utils.js

--- utils.js
+++ utils.js
@@ -1,9 +1,8 @@
 function progressBar(current, max, size = 10) {
-  const percent = current / max;
-  const filled = Math.round(size * percent);
-  const empty = size - filled;
-
-  return '🟥'.repeat(filled) + '⬜'.repeat(empty);
+    const percent = current / max;
+    const filled = Math.round(size * percent);
+    const empty = size - filled;
+    return '█'.repeat(filled) + '░'.repeat(empty);
 }
 
 module.exports = { progressBar };
