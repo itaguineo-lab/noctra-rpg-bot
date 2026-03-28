d441e7823580b8b9dfedbaefcadbfb1fb32d7a7a
itaguineo-lab <itaguineo@gmail.com>
2026-03-28 12:54:45 -0300

    Criar energy.js

---
+++ energy.js
@@ -0,0 +1,17 @@
+function updateEnergy(player) {
+  const now = Date.now();
+  const diff = Math.floor((now - player.lastEnergy) / 60000); // 1 min
+
+  if (diff > 0) {
+    player.energy = Math.min(player.energy + diff, 20);
+    player.lastEnergy = now;
+  }
+}
+
+function useEnergy(player) {
+  if (player.energy <= 0) return false;
+  player.energy -= 1;
+  return true;
+}
+
+module.exports = { updateEnergy, useEnergy };
