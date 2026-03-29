9f3f16fa12a8dec1d956831176e1e89f1c424ba8
itaguineo-lab <itaguineo@gmail.com>
2026-03-29 14:59:30 -0300

    Delete handlers directory

--- handlers/online.js
+++
@@ -1,9 +0,0 @@
-const { activeFights } = require('./combat');
-
-async function handleOnline(ctx) {
-    const onlineCount = Object.keys(activeFights).length + Math.floor(Math.random() * 50);
-    const playersOnline = Math.max(1, onlineCount);
-    await ctx.answerCbQuery(`👥 ${playersOnline} jogadores online!`, true);
-}
-
-module.exports = { handleOnline };
