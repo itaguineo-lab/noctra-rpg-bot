3c04e8d5d2dac899b9bdde9dc0dc54e7910128d2
itaguineo-lab <itaguineo@gmail.com>
2026-03-29 19:16:53 -0300

    Atualizar o equip.js

--- src/commands/equip.js
+++ src/commands/equip.js
@@ -1,4 +1,4 @@
-const { getPlayer, savePlayer, recalculateStats } = require('../../data/players');
+const { getPlayer, savePlayer, recalculateStats } = require('../data/players');
 
 async function handleEquip(ctx) {
     try {
