f5f3a53031cacaa5c669133104a24a9409dd7b71
itaguineo-lab <itaguineo@gmail.com>
2026-03-28 22:31:25 -0300

    Criar src/utils/constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+// Constantes globais
+const SLOT_EMOJIS = {
+    weapon: '⚔️', armor: '🛡️', helmet: '⛑️', boots: '👢',
+    ring: '💍', necklace: '📿', bag: '🎒'
+};
+
+const RARITY_COLORS = {
+    'Comum': '⚪',
+    'Incomum': '🟢',
+    'Raro': '🔵',
+    'Épico': '🟣',
+    'Lendário': '🟡',
+    'Mítico': '🔴'
+};
+
+const CLASSES = ['guerreiro', 'arqueiro', 'mago'];
+
+module.exports = { SLOT_EMOJIS, RARITY_COLORS, CLASSES };
