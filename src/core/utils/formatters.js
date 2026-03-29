73496b5d323b027cbbd19eb60db65e8572b5407a
itaguineo-lab <itaguineo@gmail.com>
2026-03-29 15:26:12 -0300

    Delete src/core/utils directory

--- src/core/utils/formatters.js
+++
@@ -1,6 +0,0 @@
-const { getRarityColor } = require('./constants');
-function progressBar(cur, max, size = 10) { const p = cur / max; const f = Math.round(size * p); return '█'.repeat(f) + '░'.repeat(size - f); }
-function formatNumber(n) { return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','); }
-function formatItemName(item) { if (!item) return '❓'; return `${getRarityColor(item.rarity)} *${item.name}*`; }
-function formatSoulName(soul) { if (!soul) return '❓'; return `${getRarityColor(soul.rarity)} *${soul.name}*`; }
-module.exports = { progressBar, formatNumber, formatItemName, formatSoulName };
