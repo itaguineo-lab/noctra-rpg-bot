a904972a79d402cb43d6ac6bb734eba2e35872f7
itaguineo-lab <itaguineo@gmail.com>
2026-03-28 23:39:02 -0300

    Atualizar o combat.js

--- src/handlers/combat.js
+++ src/handlers/combat.js
@@ -1,56 +1,21 @@
 const { getPlayer, savePlayer, recalculateStats } = require('../../data/players');
 const { getRandomEnemy } = require('../../data/maps');
-const { startCombat, playerAttack, playerFlee, canUseSoul, useSoul, calculateDamage } = require('../../combat');
+const { 
+    startCombat, 
+    playerAttack, 
+    playerFlee, 
+    canUseSoul, 
+    useSoul, 
+    calculateDamage 
+} = require('../../data/combatLogic');
 const { checkLevelUp } = require('../../data/level');
 const { generateItem } = require('../../data/items');
 const { dropSoul, formatSoulName } = require('../../data/souls');
-const { getRewardMultipliers, getMainMenuText, formatItemName } = require('../utils/helpers');
+const { getRewardMultipliers, getMainMenuText } = require('../utils/helpers');
+const { formatItemName } = require('../utils/formatters');
 const { combatMenu, consumablesMenu, soulsMenu } = require('../menus/combatMenu');
 const { mainMenu } = require('../menus/mainMenu');
 
 const activeFights = new Map();
 
-async function handleHunt(ctx) {
-    try {
-        const player = getPlayer(ctx.from.id);
-        // ... lógica de caçar
-    } catch (err) {
-        console.error('Erro ao caçar:', err);
-        await ctx.reply('Ocorreu um erro. Tente novamente.');
-    }
-}
-
-async function handleCombatAttack(ctx) {
-    // ... lógica de ataque
-}
-
-async function handleCombatFlee(ctx) {
-    // ... lógica de fuga
-}
-
-async function handleCombatItems(ctx) {
-    // ... lógica de itens
-}
-
-async function handleCombatSouls(ctx) {
-    // ... lógica de almas
-}
-
-async function handleUseSoul(ctx) {
-    // ... lógica de usar alma
-}
-
-async function handleUseConsumable(ctx, type, effectFn) {
-    // ... lógica de consumíveis
-}
-
-module.exports = {
-    activeFights,
-    handleHunt,
-    handleCombatAttack,
-    handleCombatFlee,
-    handleCombatItems,
-    handleCombatSouls,
-    handleUseSoul,
-    handleUseConsumable
-};
+// ... resto do código do handler (como já estava)
